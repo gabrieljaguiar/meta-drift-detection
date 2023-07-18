3c121697020fce519ca341eb2942c9519cd23cee
gabrieljaguiar <gja.gbrl@gmail.com>
2023-07-18 10:09:42 -0400

    update names

--- run_meta_experiment.sh
+++
@@ -1,4 +0,0 @@
-python adaptive_tree_meta_experiment.py --model RANDOM --mt 5000 --st 5000 --output RANDOM_results.csv --feature-set 2 --mf meta_features_set_2_stats.csv --n-jobs 64
-python adaptive_tree_meta_experiment_real_world.py --model RANDOM --mt 5000 --st 5000 --feature-set 2 --mf meta_features_set_2_stats.csv --output RANDOM_results_real_world.csv --n-jobs 64
-
-
